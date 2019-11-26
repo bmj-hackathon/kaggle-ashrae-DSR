0e945cd5a05b97896c7b3ad1355b60535f583548
MarcusJones <jones.0bj3@gmail.com>
2019-11-26 20:37:34 +0100

    update transformers

--- src/code_blocks/1_data/data_0X_features.py
+++ src/code_blocks/1_data/data_0X_features.py
@@ -1,5 +1,23 @@
-trfs.TemporalTransformer
+#%%
+def test_add_rows(row):
+    return row['building_id'] + row['meter']
 
+this_pipeline = sk.pipeline.Pipeline([
+        ('feature: Adding rows', MultipleToNewFeature(['building_id','meter'], 'Pure Breed', test_add_rows)),
+        ])
+
+logging.info("Created pipeline:")
+for i, step in enumerate(this_pipeline.steps):
+    print(i, step[0], step[1].__str__())
+
+df_trf = this_pipeline.fit_transform(train_df)
+logging.info("Pipeline complete. {} new columns.".format(len(df_all.columns) - len(original_cols)))
+
+#%%
+
+
+
+trfs.TemporalTransformer
 
 sk.impute.SimpleImputer
 sk.pipeline.FeatureUnion,
@@ -8,6 +26,9 @@ sk.preprocessing.QuantileTransformer
 sk.preprocessing.KBinsDiscretizer
 sk.preprocessing.OneHotEncoder
 sk.preprocessing.StandardScaler
+
+
+
 
 
 # categorical_features1 = ['site_id', 'meter', 'primary_use']
